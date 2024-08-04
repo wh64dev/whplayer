b969bed3074f8d79befccfd46bd12b7558b0c283
WH64 <kimpiao020@gmail.com>
2024-08-05 01:34:22 +0900

    fix: windows directory escape problem fixed

--- src/main/kotlin/net/wh64/player/util/DefMusicDir.kt
+++ src/main/kotlin/net/wh64/player/util/DefMusicDir.kt
@@ -5,7 +5,7 @@ import net.wh64.player.enum.OSType
 fun getDirectory(): String {
     val home = System.getProperty("user.home")
     return when(getOS()) {
-        OSType.WINDOWS -> "${home}\\Music"
+        OSType.WINDOWS -> "${home.replace("\\", "\\\\")}\\\\Music"
         OSType.LINUX, OSType.DARWIN -> "$home/Music"
     }
 }
